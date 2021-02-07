99a2d6b35b5809b5f7f956eeb2649c5a3cd15733
avinash273 <avinash.shanker@mavs.uta.edu>
2021-02-07 17:49:25 -0600

    first commit

---
+++ retail-app-ws/src/main/java/com/avinashdeveloper/app/ws/RetailAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.avinashdeveloper.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppWsApplication.class, args);
+	}
+
+}
---
+++ retail-app-ws/src/test/java/com/avinashdeveloper/app/ws/RetailAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinashdeveloper.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
