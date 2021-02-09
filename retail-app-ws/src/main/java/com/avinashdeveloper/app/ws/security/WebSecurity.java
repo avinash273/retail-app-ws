226b1d744f6cbc6f3be8ef779caf5999c1386313
avinash273 <avinash.shanker@mavs.uta.edu>
2021-02-08 22:05:42 -0600

    random commit

--- retail-app-ws/src/main/java/com/avinashdeveloper/app/ws/security/WebSecurity.java
+++ retail-app-ws/src/main/java/com/avinashdeveloper/app/ws/security/WebSecurity.java
@@ -2,7 +2,6 @@ package com.avinashdeveloper.app.ws.security;
 
 import com.avinashdeveloper.app.ws.service.UserService;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +22,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/users")
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
                 .anyRequest().authenticated();
     }
